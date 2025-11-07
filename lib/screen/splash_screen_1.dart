d6cf77c925830b1de1fee43999d6f7fa216f7b78
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2025-11-07 20:20:37 +0700

    menambahkan splash screen 1

---
+++ lib/screen/splash_screen_1.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatelessWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            width: 240,
+            height: 240,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.amber,
+            ),
+          ),
+
+        ],
+      ),
+        ),
+      ),
+    );
+  }
+}
